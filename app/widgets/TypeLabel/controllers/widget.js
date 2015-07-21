2945aefc468b40ed31504f0e794a7dcb8450e030
Alisson Chiquitto <chiquitto@gmail.com>
2015-07-21 16:25:09 -0300

    Pokemon Detail Type

--- app/widgets/TypeLabel/controllers/widget.js
+++ app/widgets/TypeLabel/controllers/widget.js
@@ -1,7 +1,4 @@
 var args = arguments[0] || {};
 
-Ti.API.log(JSON.stringify($.principal));
-Ti.API.log(JSON.stringify("type" + args.typeId));
-
 $.addClass($.principal, "pkTipo" + args.typeId);
 $.addClass($.label, "pkLabel" + args.typeId);
