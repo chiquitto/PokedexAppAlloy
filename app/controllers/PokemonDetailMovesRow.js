acd3c8365e270189423c55566623d0bdd5e92980
Alisson Chiquitto <chiquitto@gmail.com>
2015-07-24 15:03:56 -0300

    Pokemon Detail Moves

---
+++ app/controllers/PokemonDetailMovesRow.js
@@ -0,0 +1,8 @@
+var args = arguments[0] || {};
+
+$.lvLabel.setText('TM00' || "Row # Unknown");
+$.moveLabel.setText('Move #000' || "Row # Unknown");
+// $.typeLabel.setText('type' || "Row # Unknown");
+$.classLabel.setText('class' || "Row # Unknown");
+$.powerLabel.setText('150' || "Row # Unknown");
+$.accLabel.setText('100%' || "Row # Unknown");
