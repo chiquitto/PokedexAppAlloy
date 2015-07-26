6aa86f4ba4b25b5c7687600186249d105dc520a3
Alisson Chiquitto <chiquitto@gmail.com>
2015-07-26 16:06:38 -0300

    Pokemon Detail Screen

--- app/controllers/PokemonDetail.js
+++ app/controllers/PokemonDetail.js
@@ -1,23 +1,22 @@
-// var pokemon;
-
 Ti.App.addEventListener('pokemonDetail:selected', function(pokemon) {
-	$.pkPicture.setImage('/img/poke/00' + pokemon.getId() + '.png');
+	//alert('Chiquitto');
+
+	var number = '00' + pokemon.getId();
+	number = number.substring(number.length-3);
+	$.pkPicture.setImage('/img/poke/' + number + '.png');
 
 	$.pkName.setText('#' + pokemon.getId() + ' ' + pokemon.getIdentifier());
 
 	var abilities = pokemon.getAbilities();
 	var i;
-
-	// $.pkAbilities.removeAllChildren();
+	
 	for (i in abilities) {
-		var label = Ti.UI.createLabel({
+		var label = $.UI.create("Label", {
 			classes : ['pkAbilitie'],
-			text : abilities[i].getIdentifier() + ' ' + abilities[i].getDescription(),
+			html : '<strong>' + abilities[i].getIdentifier() + '</strong> ' + abilities[i].getDescription(),
 		});
-		//$.pkAbilities.add(label);
+		$.pkAbilities.add(label);
 	}
-
-	// $.pkAbilitie1.setHtml('<strong>Habilitie 1</strong>: Minions ipsum jiji');
 
 	$.pkDescription.setText(pokemon.getDescription());
 });
